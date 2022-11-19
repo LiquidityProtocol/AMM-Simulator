058cb0344a390177d1c03f68221c18c5c46832d4
the-nguyen <nguyentrungthentt@gmail.com>
2022-11-19 16:30:04 +0100

    Add type Token.
    Type Token has equality operator and hash function.
    
    Type Token can be used as a key type of unordered_map (i.e. hash table).

---
+++ src/Token.hpp
@@ -0,0 +1,25 @@
+#include <string>
+
+class Token {
+public:
+	Token(std::string name) : name(name) {}
+
+	std::string get_name() const {
+		return name;
+	}
+
+	bool operator==(const Token &other) const {
+		return name == other.name;
+	}
+private:
+	std::string name;
+};
+
+namespace std {
+	template<>
+	struct hash<Token> {
+		size_t operator()(const Token &token) const {
+			return hash<string>()(token.get_name());
+		}
+	};
+}
