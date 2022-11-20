42367dd305d9d65fa8e054ee4be260e4cec82659
the-nguyen <nguyentrungthentt@gmail.com>
2022-11-20 14:27:00 +0100

    Add class PoolChunk.
    PoolChunk is a chunk for one token and stores its quantity.

---
+++ src/PoolChunk.hpp
@@ -0,0 +1,21 @@
+#include "Token.hpp"
+
+class PoolChunk {
+public:
+    PoolChunk(Token token, double quantity) : token(token), quantity(quantity) {}
+
+    Token get_token() const {
+        return token;
+    }
+
+    double get_quantity() const {
+        return quantity;
+    }
+
+    void update_quantity(double change) {
+        quantity += change;
+    }
+private:
+    Token token;
+    double quantity;
+};
