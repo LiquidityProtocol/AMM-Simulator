3b75c4a7482d77de1603574910a81b9ac7fa8cb8
the-nguyen <nguyentrungthentt@gmail.com>
2022-11-20 13:24:52 +0100

    Add difference operator for type Token.

--- src/TokenTest.cpp
+++ src/TokenTest.cpp
@@ -4,7 +4,7 @@
 #include "Token.hpp"
 
 int main () {
-    Token token1("Token_1"), token2("Token_2");
+    Token token1("Token_1"), token2("Token_2"), token3("Token_1");
     
     assert(!(token1 == token2));
     std::unordered_map<Token, int> token_to_quantity;
@@ -13,6 +13,7 @@ int main () {
     assert(token_to_quantity.size() == 2);
     assert(token_to_quantity[token1] == 1);
     assert(token_to_quantity[token2] == 2);
+    assert(token1 == token3);
 
     return 0;
 }
