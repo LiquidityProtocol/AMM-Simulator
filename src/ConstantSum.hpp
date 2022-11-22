80619fdccb1d016c444496f1db202d936856599b
Adithya Ravichandran <46827614+Ad1thya-R@users.noreply.github.com>
2022-11-22 23:59:42 +0100

    Addition of Constant Sum protocol using PoolInterface

---
+++ src/ConstantSum.hpp
@@ -0,0 +1,31 @@
+#ifndef CONSTANTSUM_HPP
+#define CONSTANTSUM_HPP
+
+
+#include "PoolInterface.hpp"
+#include <stdexcept>
+
+class ConstantSum : public PoolInterface {
+public:
+    using PoolInterface::PoolInterface;
+private:
+    double GetT1inT2Price(Token *token1, Token *token2 ) const {
+        if (!InPool(token1) || !InPool(token2)) {
+            throw std::invalid_argument("invalid token");
+        }
+        return (quantities_.find(token2)->second)/(quantities_.find(token1)->second);
+    }
+
+    double ExecuteSwap(Token *input_token, Token *output_token, double input_quantity) {
+
+        double output_quantity = GetT1inT2Price(input_token, output_token)*input_quantity;
+        if (quantities_[output_token] < output_quantity) {
+            throw std::invalid_argument("Too few tokens in pool for swap");
+        }
+        quantities_[input_token] += input_quantity;
+        quantities_[output_token] -= output_quantity;
+        return output_quantity;
+    }
+};
+
+#endif // CONSTANTSUM_HPP
