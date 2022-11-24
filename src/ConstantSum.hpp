f6048ec910a35f93aad214c25edd72025ed17c24
the-nguyen <nguyentrungthentt@gmail.com>
2022-11-24 22:05:07 +0100

    Fix ConstantSum.

--- src/ConstantSum.hpp
+++ src/ConstantSum.hpp
@@ -1,28 +1,22 @@
-#ifndef CONSTANTSUM_HPP
-#define CONSTANTSUM_HPP
+#ifndef CONSTANT_SUM_HPP
+#define CONSTANT_SUM_HPP
 
-
-#include "PoolInterface.hpp"
 #include <stdexcept>
+#include "PoolInterface.hpp"
 
 class ConstantSum : public PoolInterface {
 public:
     using PoolInterface::PoolInterface;
 private:
-    double GetT1inT2Price(Token *token1, Token *token2 ) const {
-        return (quantities_.find(token2)->second)/(quantities_.find(token1)->second);
-    }
-
     double ExecuteSwap(Token *input_token, Token *output_token, double input_quantity) {
-
-        double output_quantity = GetT1inT2Price(input_token, output_token)*input_quantity;
-        if (quantities_[output_token] < output_quantity) {
-            throw std::invalid_argument("Too few tokens in pool for swap");
+        if (input_quantity > quantities_[output_token]) {
+            throw std::invalid_argument("provided too many " + input_token->name() + "s");
         }
+        double output_quantity = input_quantity;
         quantities_[input_token] += input_quantity;
         quantities_[output_token] -= output_quantity;
         return output_quantity;
     }
 };
 
-#endif // CONSTANTSUM_HPP
+#endif // CONSTANT_SUM_HPP
--- src/ConstantSumTest.cpp
+++ src/ConstantSumTest.cpp
@@ -1,32 +1,27 @@
 #include <cassert>
-#include <unordered_set>
-#include "Token.hpp"
+#include <stdexcept>
 #include "ConstantSum.hpp"
-#include "PoolInterface.hpp"
 
 int main() {
     Token *token1 = Token::GetToken("token1");
     Token *token2 = Token::GetToken("token2");
-    Token *token3 = Token::GetToken("token3");
 
     ConstantSum pool({{token1, 10}, {token2, 20}});
 
-    assert(pool.InPool(token1));
-    assert(!pool.InPool(token3));
+    assert(pool.Swap(token1, token2, 10) == 10);
+    assert(pool.GetQuantity(token1) == 20);
+    assert(pool.GetQuantity(token2) == 10);
+
+    assert(pool.Swap(token1, token2, 10) == 10);
+    assert(pool.GetQuantity(token1) == 30);
+    assert(pool.GetQuantity(token2) == 0);
 
     try {
-        pool.GetQuantity(token3);
+        pool.Swap(token1, token2, 10);
         assert(false);
     } catch (std::invalid_argument &e) {
-        assert(std::string(e.what()) == "invalid token");
+        assert(std::string(e.what()) == "provided too many token1s");
     }
-    assert(pool.GetQuantity(token1) == 10);
-
-    assert(pool.tokens() == std::unordered_set<Token *>({token1, token2}));
-
-    assert(pool.Swap(token1, token2, 10) == 20);
-    assert(pool.GetQuantity(token1) == 20);
-    assert(pool.GetQuantity(token2) == 0);
 
     return 0;
 }
