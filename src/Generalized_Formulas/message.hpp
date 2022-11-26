56023ae3dacb8e977d2b9a957eaa3cbc1302ed56
Combi2k2 <cumbee2k2@gmail.com>
2022-11-26 03:25:47 +0100

    Print message when pass the test

--- src/Generalized_Formulas/message.hpp
+++ src/Generalized_Formulas/message.hpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-void alert(bool condition, std::string passMessage, std::string failMessage)  {
+void alert(bool condition, std::string failMessage, std::string passMessage = "")   {
     if (!condition) {
         std::cerr << "\033[31m" << failMessage + "\033[0m\n";
         exit(0);
     }
-    else
+    else if (passMessage != "")
         std::cerr << "\033[32m" << passMessage + "\033[0m\n";
 }
