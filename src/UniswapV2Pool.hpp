462420ede2119f5c47be7a887236c73b67d250f8
Combi2k2 <cumbee2k2@gmail.com>
2022-11-27 13:33:45 +0100

    Add slippage and divergence loss API
    Create new function to compute swapped quantity (no change to the quantity attribute of the Pool)

--- src/UniswapV2Pool.hpp
+++ src/UniswapV2Pool.hpp
@@ -8,10 +8,19 @@ public:
     using PoolInterface::PoolInterface;
 private:
     double ExecuteSwap(Token *input_token, Token *output_token, double input_quantity) {
-        double output_quantity = quantities_[output_token] - quantities_[input_token] * quantities_[output_token] / (quantities_[input_token] + (1 - pool_fee_) * input_quantity);
+        double output_quantity = ComputeSwapQuantity(input_token, output_token, input_quantity);
         quantities_[input_token] += input_quantity;
         quantities_[output_token] -= output_quantity;
         return output_quantity;
+    }
+    double ComputeSwapQuantity(Token *input_token, Token *output_token, double input_quantity)  {
+        return quantities_[output_token] - quantities_[input_token] * quantities_[output_token] / (quantities_[input_token] + (1 - pool_fee_) * input_quantity);
+    }
+    double ComputeSlippage(Token *input_token, Token *output_token, double input_quantity)  {
+        return  input_quantity / quantities_[input_token] - 1;
+    }
+    double ComputeDivergenceLoss(double p, Token *token)    {
+        return  sqrt(1 + p) / (1 + p/2) - 1;
     }
 };
 
