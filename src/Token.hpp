5376e50ed85e7c3eb9fbabf72b4f45ea9b60cc54
the-nguyen <nguyentrungthentt@gmail.com>
2022-12-02 21:53:00 +0100

    Revert "Refactor Token."
    This reverts commit f1cbd61bae719fcc4aaa625d442746782a5dd18d.

--- src/Token.hpp
+++ src/Token.hpp
@@ -10,18 +10,32 @@ public:
 	Token & operator=(const Token &) = delete;
 	Token(const Token &) = delete;
 
-	static Token * GetToken(const std::string &name);
+	static Token * GetToken(const std::string &name) {
+		if (!existing_tokens_.count(name)) {
+			existing_tokens_[name] = new Token(name);
+		}
+		return existing_tokens_[name];
+	}
 
-	std::string name() const;
+	std::string name() const {
+		return name_;
+	}
 
-	double real_value() const;
-	void set_real_value(double real_value);
+	double real_value() const {
+		return real_value_;
+	}
+
+	void set_real_value(double real_value) {
+		real_value_ = real_value;
+	}
 private:
-	Token(const std::string &name);
+	Token(const std::string &name) : name_(name), real_value_(0) {}
 
 	static std::unordered_map<std::string, Token *> existing_tokens_;
 	std::string name_;
 	double real_value_;
 };
+
+std::unordered_map<std::string, Token *> Token::existing_tokens_ = std::unordered_map<std::string, Token *>();
 
 #endif
