7e5380acae8ac873e06e02969876335a1676670b
Combi2k2 <cumbee2k2@gmail.com>
2022-12-03 05:21:56 +0100

    Fix the include path

--- src/Protocols/BalancerPool.hpp
+++ src/Protocols/BalancerPool.hpp
@@ -4,7 +4,7 @@
 #include <unordered_map>
 #include <stdexcept>
 #include <cmath>
-#include "PoolInterface.hpp"
+#include "../PoolInterface.hpp"
 #include "Token.hpp"
 
 class BalancerPool : public PoolInterface {
--- src/Protocols/ConstantSum.hpp
+++ src/Protocols/ConstantSum.hpp
@@ -1,31 +1,31 @@
 #ifndef CONSTANTSUM_HPP
- #define CONSTANTSUM_HPP
+#define CONSTANTSUM_HPP
 
 
- #include "PoolInterface.hpp"
- #include <stdexcept>
+#include "../PoolInterface.hpp"
+#include <stdexcept>
 
- class ConstantSum : public PoolInterface {
-     /*Implementation of modified constant sum protocol:
-      Description of Protocol: px+y=k; p is price when pool is initialised, x and y tokens respectively
-     */
- public:
-     using PoolInterface::PoolInterface;
- private:
-     double GetT1inT2Price(Token *token1, Token *token2 ) const {
-         return (quantities_.find(token2)->second)/(quantities_.find(token1)->second);
-     }
+class ConstantSum : public PoolInterface {
+    /*Implementation of modified constant sum protocol:
+    Description of Protocol: px+y=k; p is price when pool is initialised, x and y tokens respectively
+    */
+public:
+    using PoolInterface::PoolInterface;
+private:
+    double GetT1inT2Price(Token *token1, Token *token2 ) const {
+        return (quantities_.find(token2)->second)/(quantities_.find(token1)->second);
+    }
 
-     double ExecuteSwap(Token *input_token, Token *output_token, double input_quantity) {
+    double ExecuteSwap(Token *input_token, Token *output_token, double input_quantity) {
 
-         double output_quantity = GetT1inT2Price(input_token, output_token)*input_quantity;
-         if (quantities_[output_token] < output_quantity) {
-             throw std::invalid_argument("Too few tokens in pool for swap");
-         }
-         quantities_[input_token] += input_quantity;
-         quantities_[output_token] -= output_quantity;
-         return output_quantity;
-     }
- };
+        double output_quantity = GetT1inT2Price(input_token, output_token)*input_quantity;
+        if (quantities_[output_token] < output_quantity) {
+            throw std::invalid_argument("Too few tokens in pool for swap");
+        }
+        quantities_[input_token] += input_quantity;
+        quantities_[output_token] -= output_quantity;
+        return output_quantity;
+    }
+};
 
- #endif // CONSTANTSUM_HPP
+#endif // CONSTANTSUM_HPP
