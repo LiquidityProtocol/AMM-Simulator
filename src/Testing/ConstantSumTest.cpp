3170d27179539ab8f5e25c7939dacdce97b3d566
Combi2k2 <cumbee2k2@gmail.com>
2022-12-04 15:27:42 +0100

    Done testing ConstantSum protocol

--- src/Testing/ConstantSumTest.cpp
+++ src/Testing/ConstantSumTest.cpp
@@ -1,27 +1,32 @@
 #include <cassert>
 #include <stdexcept>
-#include "ConstantSum.hpp"
-#include "Token.hpp"
+#include "../Protocols/ConstantSum.hpp"
 
 int main() {
     Token *token1 = Token::GetToken("token1");
     Token *token2 = Token::GetToken("token2");
 
-    ConstantSum pool({{token1, 10}, {token2, 20}});
+    ConstantSum pool({{token1, 50}, {token2, 50}},
+                     {{token1, 10}, {token2, 10}}, 0);
 
-    assert(pool.Swap(token1, token2, 10) == 10);
-    assert(pool.GetQuantity(token1) == 20);
+    Account *combi = Account::GetAccount("Combi");
+
+    combi -> Deposit(token1, 100);
+    combi -> Deposit(token2, 100);
+
+    assert(pool.Swap(combi, token1, token2, 20) == 20);
+    assert(pool.GetQuantity(token1) == 70);
+    assert(pool.GetQuantity(token2) == 30);
+
+    assert(pool.Swap(combi, token1, token2, 20) == 20);
+    assert(pool.GetQuantity(token1) == 90);
     assert(pool.GetQuantity(token2) == 10);
 
-    assert(pool.Swap(token1, token2, 10) == 10);
-    assert(pool.GetQuantity(token1) == 30);
-    assert(pool.GetQuantity(token2) == 0);
-
     try {
-        pool.Swap(token1, token2, 10);
+        pool.Swap(combi, token1, token2, 20);
         assert(false);
     } catch (std::invalid_argument &e) {
-        assert(std::string(e.what()) == "provided too many token1s");
+        assert(std::string(e.what()) == "Not enough liquidity");
     }
 
     return 0;
