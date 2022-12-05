573aa076b74139fcb644ff5cc6d955e19118156f
Combi2k2 <cumbee2k2@gmail.com>
2022-12-05 21:16:02 +0100

    Rewrite the ostream operator of this class

--- src/Utilities/Operation.cpp
+++ src/Utilities/Operation.cpp
@@ -3,27 +3,33 @@
 
 using namespace std;
 
-Operation::Operation(const std::string &type, std::string account, PoolInterface * pool, std::unordered_map<Token *, double> input, std::unordered_map<Token *, double> output) {
-        operation_type_ = type;
-        account_ = account;
-        pool_ = pool;
-        input_ = input;
-        output_ = output;
-    };
+Operation::Operation(const std::string &type,
+                     const std::string &account,
+                     PoolInterface * pool,
+                     std::unordered_map<Token *, double> input,
+                     std::unordered_map<Token *, double> output) {
+    operation_type_ = type;
+    account_ = account;
+    pool_ = pool;
+    input_ = input;
+    output_ = output;
+};
 
-void Operation::Print() {
-        cout << "OPERATION " << to_string(reinterpret_cast<uint64_t>(this)) << ":\n";
-        cout << ">>> TYPE: " << operation_type_ << "\n";
-        cout << ">>> ACCOUNT: " << account_ << "\n";
-        cout << ">>> POOL: " << pool_ << "\n";
-        cout << ">>> INPUT: ";
-        for (auto [token, quantity]: input_){
-            cout << token->name() << ": " << std::to_string(quantity) << "; ";
-        }
-        cout << "\n";
-        cout << ">>> OUTPUT: ";
-        for (auto [token, quantity]: output_){
-            cout << token->name() << ": " << std::to_string(quantity) << "; ";
-        }
-        cout << "\n";
-    };
+std::ostream& operator << (std::ostream& os, const Operation& ops)  {
+    os << "OPERATION " << to_string(reinterpret_cast<uint64_t>(&ops)) << ":\n";
+    os << ">>> TYPE: " << ops.operation_type_ << "\n";
+    os << ">>> ACCOUNT: " << ops.account_ << "\n";
+    os << ">>> POOL: " << ops.pool_ << "\n";
+    os << ">>> INPUT: ";
+
+    for(auto [token, quantity] : ops.input_)
+        os << token->name() << ": " << quantity << "; ";
+    
+    os << "\n";
+    os << ">>> OUTPUT: ";
+    for (auto [token, quantity]: ops.output_)
+        os << token->name() << ": " << quantity << "; ";
+    
+    os << "\n\n";
+    return  os;
+}
