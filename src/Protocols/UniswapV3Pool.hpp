5c6c97292c16f220b34880418ce66e9a66cbb462
combi2k2 <cumbee2k2@gmail.com>
2022-12-18 15:07:48 +0100

    Fix the order of constants associated with each token

--- src/Protocols/UniswapV3Pool.hpp
+++ src/Protocols/UniswapV3Pool.hpp
@@ -9,38 +9,49 @@ public:
     UniswapV3Pool(std::unordered_map<Token *, double> quantities,
                   double pool_fee, 
                   double slippage_controller) : PoolInterface(quantities, pool_fee) {
-        if(slippage_controller<1){
+        if (slippage_controller < 1) {
             throw std::invalid_argument("invalid slippage controller");
-        }
-        else{
+        } else {
             slippage_controller_ = slippage_controller;
         }
-    
-        double r1 = quantities.begin() -> second;
-        double r2 = (++quantities.begin()) -> second;
-
-        double C1 = r1;
-        double C2 = r2;
+        weights_ = quantities;
+    }
+    double getWeight(Token *token) const {
+        return weights_.find(token) -> second;
     }
 
 private:
-    double C1, C2;
+    std::unordered_map<Token *, double> weights_;
     double slippage_controller_;
 
     double ComputeInvariant() const {
-        return slippage_controller_ * C1 * C2 /((sqrt(slippage_controller_) - 1) * (sqrt(slippage_controller_) - 1));
+        double ans = 1;
+
+        for(auto [token, weight] : weights_)
+            ans *= weight;
+        
+        ans *= slippage_controller_;
+        ans /= (sqrt(slippage_controller_) - 1);
+        ans /= (sqrt(slippage_controller_) - 1);
+
+        return ans;
     }
     double ComputeSpotExchangeRate(Token *input_token, Token *output_token) const {
-        double r1 = GetQuantity(input_token);
-        double r2 = GetQuantity(output_token);
-        return (r1 + C1/(sqrt(slippage_controller_) - 1))/(r2 + C2/(sqrt(slippage_controller_) - 1));
+        double r1 = GetQuantity(input_token), C1 = getWeight(input_token);
+        double r2 = GetQuantity(output_token), C2 = getWeight(output_token);
+
+        return (r1 + C1 / (sqrt(slippage_controller_) - 1)) / (r2 + C2 / (sqrt(slippage_controller_) - 1));
     }
     double ComputeSwappedQuantity(Token *input_token, Token *output_token, double input_quantity) const {
-        double r1 = GetQuantity(input_token);
-        double r2 = GetQuantity(output_token);
+        double r1 = GetQuantity(input_token), C1 = getWeight(input_token);
+        double r2 = GetQuantity(output_token), C2 = getWeight(output_token);
+
+        double B = sqrt(slippage_controller_) - 1;
+
         double r1_prime = (r1 + input_quantity);
-        double B = (1-1/sqrt(slippage_controller_))*(1-1/sqrt(slippage_controller_));
-        return  r2 - (C1*C2/(B*(r1_prime + C1/(sqrt(slippage_controller_)-1))) - C2/(sqrt(slippage_controller_)-1));
+        double r2_prime = ComputeInvariant() / (r1_prime + C1 / B) - C2 / B;
+
+        return r2 - r2_prime;
     }
 };
 
