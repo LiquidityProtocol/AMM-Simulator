4e1cea36f43a995a060635c453d882f0658d81d9
Matheo-LeMasson <matheo.le-masson@polytechnique.edu>
2022-12-27 13:49:18 +0100

    error message if invalid input

--- MintDialog.cpp
+++ MintDialog.cpp
@@ -2,6 +2,7 @@
 #include "ui_MintDialog.h"
 #include "AccountListWidgetItem.h"
 #include "src/Playground.hpp"
+#include <QMessageBox>
 
 MintDialog::MintDialog(QWidget *parent, Playground *playground) :
     QDialog(parent),
@@ -23,5 +24,34 @@ MintDialog::~MintDialog()
 
 void MintDialog::on_pushButton_clicked()
 {
-    emit SendData(playground_->GetToken(ui->comboBox->currentText().toStdString()).first, ui->lineEdit->text().toDouble());
+   QString userinput = ui->lineEdit->text();
+   int flag = 0;
+   int error = 0;
+   /*QList<QChar> chars;
+   for (QChar c: userinput) {
+       chars.push_back(c);
+    }*/
+   QList<QChar> digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+   /*for (int k=0; k < 10; k++) {
+       digits.push_back(char(k));
+   }
+   for (auto const &i: digits) {
+       std::cout << &i << std::endl;
+   }*/
+   for (int i = 0; i < userinput.size(); i++) {
+      if (userinput[i] == '.') {
+          flag += 1;
+      }
+      else if ( (std::find(digits.begin(), digits.end(), userinput[i]) != digits.end()) != true ) {
+          error += 1;
+      }
+   }
+
+   if (flag > 1 || error != 0) {
+       /*QMessageBox::about(this, "Minting failed", "Invalid input!");*/
+       QMessageBox::about(this, "Minting token failed", "Please mint a number of tokens!");
+   }
+   else {
+       emit SendData(playground_->GetToken(ui->comboBox->currentText().toStdString()).first, ui->lineEdit->text().toDouble());
+   }
 }
