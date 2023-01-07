64dffbaf7677b1065f9363c507c04db0db6d372f
Kaoutar-Fahraoui <kaoutar.fahraoui@polytechnique.edu>
2023-01-07 18:19:25 +0100

    Remodification of sample uniform distribution

--- src/Utilities/Unif_Distrib_random.cpp
+++ src/Utilities/Unif_Distrib_random.cpp
@@ -2,14 +2,14 @@
 #include <random>
 
 double sample_uniform_distribution(double mean, double variance) {
-  // Create a uniform distribution with the given mean and variance
-  std::uniform_real_distribution<double> distribution(mean - variance / 2, mean + variance / 2);
+    // Get a random number generator
+    std::random_device rd;
+    std::mt19937 generator(rd());
 
-  // Get a random number generator
-  std::random_device rd;
-  std::mt19937 generator(rd());
+    // Create a uniform distribution with the given mean and variance
+    std::uniform_real_distribution<double> distribution(mean - variance / 2, mean + variance / 2);
 
-  // Generate and return a sample from the distribution
-  return distribution(generator);
-}
+    // Generate and return a sample from the distribution
+    return distribution(generator);
+    }
 
