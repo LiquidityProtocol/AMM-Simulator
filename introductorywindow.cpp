5d809ae0d16e91b0ecb13949472d2cb51282ad69
Jasmine-Samson <110972762+Jasmine-Samson@users.noreply.github.com>
2023-01-12 11:54:30 +0100

    Changes on the introductory window design (2 tables))

--- introductorywindow.cpp
+++ introductorywindow.cpp
@@ -1,12 +1,17 @@
 #include "introductorywindow.h"
 #include "ui_introductorywindow.h"
 #include "MainWindow.h"
+#include  <QPixmap>
 
 IntroductoryWindow::IntroductoryWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::IntroductoryWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/jasmine/Desktop/LP_logo.png");
+    //int w = ui->label_pic->width();
+    //int h = ui->label_pic->height();
+    ui->label_pic->setPixmap(pix.scaled(50,50,Qt::KeepAspectRatio));
 }
 
 IntroductoryWindow::~IntroductoryWindow()
