99523ec639487635f2626b6af3ac450bb1e15e3e
Kaoutar-Fahraoui <kaoutar.fahraoui@polytechnique.edu>
2023-01-12 09:54:04 +0100

    fixed uniform generator

--- src/Utilities/Unif_Distrib_random.cpp
+++ src/Utilities/Unif_Distrib_random.cpp
@@ -1,14 +1,17 @@
 #include <cmath>
 #include <random>
+#include <iostream>
 
-uniform_distribution_generator::uniform_distribution_generator(double mean, double variance, int sample_size)
+double uniform_distribution_generator(double mean, double variance, std::string identifier)
 {
     double lower_bound = mean - sqrt(3 * variance);
     double upper_bound = mean + sqrt(3 * variance);
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_real_distribution<> d(lower_bound, upper_bound);
-    return d(gen);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(lower_bound, upper_bound);
+    double random_number = dis(gen);
+    std::cout << "Generated a random number with identifier: " << identifier << std::endl;
+    return random_number;
 }
 
 
