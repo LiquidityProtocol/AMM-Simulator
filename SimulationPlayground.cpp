a276437cf1d33ccc4a88c0f0da03e772c20dc81a
Matheo-LeMasson <matheo.le-masson@polytechnique.edu>
2023-01-12 12:15:58 +0100

    first steps to get account

--- SimulationPlayground.cpp
+++ SimulationPlayground.cpp
@@ -18,6 +18,13 @@ SimulationPlayground::SimulationPlayground(QWidget *parent) :
         item->setSizeHint(pool_graph->sizeHint());
         ui->listWidget->setItemWidget(item, pool_graph);
     }
+    for (auto account: market_->GetMarketAccounts()){
+        QListWidgetItem *item = new QListWidgetItem(ui->listWidget2);
+        ui->listWidget2->addItem(item);
+        PoolGraphItem *pool_graph2 = new PoolGraphItem(this, account);
+        item->setSizeHint(pool_graph2->sizeHint());
+        ui->listWidget2->setItemWidget(item, pool_graph2);
+    }
 
     test_token = market_->getToken("UNI");
 //    token_to_graph[test_token] = ui->widget_pool_inventory->addGraph();
@@ -34,6 +41,12 @@ void SimulationPlayground::on_pushButton_clicked()
 {
     market_->runEpoch();
     for(int i = 0; i < ui->listWidget->count(); ++i){
+        QListWidgetItem *item = ui->listWidget->item(i);
+        QWidget *item_widget = ui->listWidget->itemWidget(item);
+        PoolGraphItem *pool_graph = qobject_cast<PoolGraphItem *>(item_widget);
+        pool_graph->UpdateGraph();
+    }
+    for(int i = 0; i < ui->listWidget2->count(); ++i){
         QListWidgetItem *item = ui->listWidget->item(i);
         QWidget *item_widget = ui->listWidget->itemWidget(item);
         PoolGraphItem *pool_graph = qobject_cast<PoolGraphItem *>(item_widget);
