69336fc9a8dc00b199634071167996407610fce7
the-nguyen <nguyentrungthentt@gmail.com>
2023-01-19 15:15:44 +0100

    Refactor AccountItem

--- AccountListWidgetItem.cpp
+++ AccountListWidgetItem.cpp
@@ -27,37 +27,35 @@ Account *AccountListWidgetItem::account() const
     return account_;
 }
 
-void AccountListWidgetItem::VerifyMintRequest(Token *token, double quantity)
-{
-    try {
-        account_->Deposit(token, quantity);
-        ui->lineEdit_2->setText(QString::number(account_->total_value()));
-        UpdateWallet();
-        mint_dialog->accept();
-    } catch (std::exception &e) {
-        QMessageBox::about(mint_dialog, "Mint failed", e.what());
-    }
-}
-
 void AccountListWidgetItem::on_mint_pushButton_clicked()
 {
     if (!playground_->existing_tokens().size()) {
-        QMessageBox::about(this, "Minting failed", "There are no tokens at the moment!");
+        QMessageBox::about(this, "Mint failed", "There are no tokens at the moment!");
         return;
     }
     mint_dialog = new MintDialog(this, playground_);
+    mint_dialog->setWindowTitle(QString::fromStdString("Mint to account " + account_->name()));
     mint_dialog->exec();
 }
 
 void AccountListWidgetItem::on_trade_pushButton_clicked()
 {
     trade_dialog = new TradeDialog(this, playground_, account_);
+    trade_dialog->setWindowTitle(QString::fromStdString("Trade for account " + account_->name()));
     trade_dialog->exec();
+}
+
+void AccountListWidgetItem::on_provide_pushButton_clicked()
+{
+    provide_dialog = new ProvideDialog(this);
+    provide_dialog->setWindowTitle(QString::fromStdString("Provide from account " + account_->name()));
+    provide_dialog->exec();
 }
 
 void AccountListWidgetItem::on_withdraw_pushButton_clicked()
 {
     withdraw_dialog = new WithdrawDialog(this, playground_, account_);
+    withdraw_dialog->setWindowTitle(QString::fromStdString("Withdraw from account " + account_->name()));
     withdraw_dialog->exec();
 }
 
@@ -70,6 +68,18 @@ void AccountListWidgetItem::UpdateWallet()
         WalletListWidgetItem *wallet_item = new WalletListWidgetItem(this, token, quantity);
         item->setSizeHint(wallet_item->sizeHint());
         ui->listWidget->setItemWidget(item, wallet_item);
+    }
+}
+
+void AccountListWidgetItem::VerifyMintRequest(Token *token, double quantity)
+{
+    try {
+        account_->Deposit(token, quantity);
+        ui->lineEdit_2->setText(QString::number(account_->total_value()));
+        UpdateWallet();
+        mint_dialog->accept();
+    } catch (std::exception &e) {
+        QMessageBox::about(mint_dialog, "Mint failed", e.what());
     }
 }
 
@@ -159,10 +169,4 @@ void AccountListWidgetItem::VerifyExistingProvideRequest(PROTOCOL protocol, cons
     }  catch (std::exception &e) {
         QMessageBox::about(existing_pool_provision_dialog, "Provide failed", e.what());
     }
-}
-
-void AccountListWidgetItem::on_provide_pushButton_clicked()
-{
-    provide_dialog = new ProvideDialog(this);
-    provide_dialog->exec();
 }
