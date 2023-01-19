cb343da5c28b8a2ff1f8b928ce1cebdc1472a3d8
combi2k2 <cumbee2k2@gmail.com>
2023-01-19 11:33:05 +0100

    Add Simulation class

--- src/Market/Simulation.cpp
+++ src/Market/Simulation.cpp
@@ -1,5 +1,42 @@
 #include "Simulation.hpp"
+#include <QThread>
 
 Simulation::Simulation() {
+    market_ = new Market();
+    handler = new SignalsHandler();
+    arb_ = new Arbitrager("Market Arb", 10000);
+    LP_ = new Provider("Market LP", 100000);
+}
 
+std::unordered_set<PoolInterface *> Simulation::GetPools() const {
+    return market_->GetMarketPools();
+}
+std::unordered_set<Token *> Simulation::GetTokens() const {
+    return market_->GetMarketTokens();
+}
+
+Market *Simulation::GetMarket() const {
+    return market_;
+}
+
+Account *Simulation::GetArb() const {
+    return arb_;
+}
+Account *Simulation::GetLP() const {
+    return LP_;
+}
+
+
+
+
+void Simulation::runEpoch() {
+    for (auto pool : GetPools()) {
+        arb_->StrategicTrade(pool);
+        LP_->StrategicProvide(pool);
+    }
+    market_->runEpoch();
+    handler->respondSignals();
+
+    for (auto pool : GetPools())
+        pool->endEpoch();
 }
