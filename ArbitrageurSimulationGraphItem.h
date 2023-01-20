c1ada844e56fa3d7dea19b4af84c93db78c7e576
Matheo-LeMasson <matheo.le-masson@polytechnique.edu>
2023-01-20 14:03:19 +0100

    Update ArbitrageurSimulationGraphItem.h

--- ArbitrageurSimulationGraphItem.h
+++ ArbitrageurSimulationGraphItem.h
@@ -1,22 +1,24 @@
 #ifndef ARBITRAGEURSIMULATIONGRAPHITEM_H
 #define ARBITRAGEURSIMULATIONGRAPHITEM_H
-
-#include <QWidget>
+#include <QDialog>
 
 namespace Ui {
 class ArbitrageurSimulationGraphItem;
 }
 
-class ArbitrageurSimulationGraphItem : public QWidget
+class ArbitrageurSimulationGraphItem : public QDialog
 {
     Q_OBJECT
 
 public:
-    explicit ArbitrageurSimulationGraphItem(QWidget *parent = nullptr);
+    explicit ArbitrageurSimulationGraphItem(QWidget *parent = nullptr, QVector<double> epochs={}, QVector<double> wallet_values={});
     ~ArbitrageurSimulationGraphItem();
 
 private:
     Ui::ArbitrageurSimulationGraphItem *ui;
+    QVector<double> epochs;
+    QVector<double> wallet_values_;
 };
 
 #endif // ARBITRAGEURSIMULATIONGRAPHITEM_H
+
